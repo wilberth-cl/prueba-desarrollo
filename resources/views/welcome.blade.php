04e140d86ff192f18f2e2de0c526a88ff3e11971
wilberth-cl <wilberthfrancisco.cl@gmail.com>
2023-02-15 17:02:18 -0600

    Update Details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -445,7 +445,7 @@
             </div>
 
             <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">prueba-desarrollo</div>
+                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">Shopping Cart</div>
                 <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </div>
